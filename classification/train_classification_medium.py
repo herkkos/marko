d8f98cf5d08cd17cfca4e7a342b3cde19f662f06
Herkko Salonen <herkko.salonen@hotmail.fi>
2022-08-29 20:20:54 +0300

    Stabilize training

--- classification/train_classification_medium.py
+++ classification/train_classification_medium.py
@@ -1,28 +1,25 @@
 import random
 
 import numpy as np
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import  Dense, Dropout, BatchNormalization, LSTM, PReLU, Input, Concatenate
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 
 
-REUSES = 5
 BATCH_SIZE = 128
 EPOCHS = 50
 HISTORY_VAR = 5
 PRED_LENGTH = 15
 N_CATEGORIES = 3205
-N_SPLITS = 50
+N_SPLITS = 100
+TRAIN_SPLITS = 50
 
-RESPONSE_MODEL = 'marko_response_medium.h5'
-C_FILE = 'categories_medium.txt'
-CATEGORY_BATCH_SIZE = 5000
-
-CHARS = ' abcdefghijklmnopqrstuvwxyzåäö'
+MODEL_NAME = 'marko_response_medium.h5'
+C_FILE = '../categories_medium.txt'
 
 
-def main():      
+def create_model():
     # Input
     inputA = Input(shape=(HISTORY_VAR, N_CATEGORIES,))
     inputB = Input(shape=(PRED_LENGTH, N_CATEGORIES))
@@ -48,53 +45,83 @@ def main():
     model = Model(inputs=[x.input, y.input], outputs=z)
     opt = Adam(learning_rate=0.0001)
     model.compile(opt, 'categorical_crossentropy', metrics=['accuracy'])
+    return model
 
+def load_clf_model(model_name: str):
+    #TODO: use folder instead of file
+    #TODO: find newest model file from folder
+    try:
+        model = load_model(model_name)
+        print(f"Loaded newest model: {model_name}")
+    except:
+        model = create_model()
+        print("Training new model from scratch")
+
+    return model
+
+def train_split(split_idx: int, split_size: int):
+    print(f"Training random index: {split_idx}")
+    model = load_clf_model(MODEL_NAME)
+    
+    with open(C_FILE, 'r') as f:
+        categories = f.readlines()
+        
+    split_categories = categories[split_idx*split_size : ((split_idx+1)*split_size) - 1]
+    del categories
+    
+    class_preds = []
+    for line in split_categories:
+        if line.strip():
+            class_preds.append([int(x) for x in line.strip().split(',')])
+    
+    XA = [] # Previous messages
+    XB = [] # New words one-hot
+    y = []
+    for pred_idx in range(HISTORY_VAR, len(class_preds) - 1):
+        category_mat = []
+        for prev_category_idx in reversed(range(1, HISTORY_VAR + 1)):
+            classes_one_hot = np.zeros(N_CATEGORIES)
+            for _class in class_preds[pred_idx-prev_category_idx]:
+                classes_one_hot[_class] = 1
+            category_mat.append(classes_one_hot)
+
+        xx = np.array([[0] * (N_CATEGORIES - 1) + [1]] * PRED_LENGTH)
+        class_categories = class_preds[pred_idx][:PRED_LENGTH] + [N_CATEGORIES - 1]
+        for cat_idx, cat in enumerate(class_categories):
+            XA.append(category_mat)
+            XB.append(xx)
+            y.append(to_categorical(cat, N_CATEGORIES))
+            xx = np.vstack([xx, to_categorical(cat, N_CATEGORIES)])[1:]
+
+    XA = np.array(XA, dtype=np.float32)
+    XB = np.array(XB).astype(np.float32)
+    y = np.array(y).astype(np.float32)
+
+    #TODO: validation set
+    model.fit(x=[XA, XB],
+              y=y,
+              epochs=EPOCHS,
+              batch_size=BATCH_SIZE,
+              shuffle=True)
+    
+    model.save(MODEL_NAME)
+
+def main():      
     with open(C_FILE, 'r') as f:
         categories = f.readlines()
 
-    class_preds = []
-    for line in categories:
-        if line.strip():
-            class_preds.append([int(x) for x in line.strip().split(',')])
-
-    split_array = list(range(N_SPLITS))
-    random.shuffle(split_array)
-    split_size = len(class_preds) // N_SPLITS
-    for split_idx in split_array:
-        split_preds = class_preds[split_idx*split_size : ((split_idx+1)*split_size) - 1]
-
-        XA = [] # Previous messages
-        XB = [] # New words one-hot
-        y = []
-        for pred_idx in range(HISTORY_VAR, len(split_preds) - 1):
-            category_mat = []
-            for prev_category_idx in reversed(range(1, HISTORY_VAR + 1)):
-                classes_one_hot = np.zeros(N_CATEGORIES)
-                for _class in split_preds[pred_idx-prev_category_idx]:
-                    classes_one_hot[_class] = 1
-                category_mat.append(classes_one_hot)
-
-            xx = np.array([[0] * (N_CATEGORIES - 1) + [1]] * PRED_LENGTH)
-            for cat_idx, cat in enumerate(split_preds[pred_idx][:PRED_LENGTH]):
-                XA.append(category_mat)
-                XB.append(xx)
-                y.append(to_categorical(cat, N_CATEGORIES))
-                xx[-(cat_idx + 1)] = to_categorical(cat, N_CATEGORIES)
-
-        XA = np.array(XA, dtype=np.float32)
-        XB = np.array(XB).astype(np.float32)
-        y = np.array(y).astype(np.float32)
-
-        # TODO: validation set & outlier analysis -> outlier deletion because they probably contain silly msgs
-
-        model.fit(x=[XA, XB],
-                  y=y,
-                  epochs=EPOCHS,
-                  batch_size=BATCH_SIZE,
-                  shuffle=True)
+    split_size = len(categories) // N_SPLITS
+    del categories
     
-    model.save(RESPONSE_MODEL)
+    for epoch in range(TRAIN_SPLITS):
+        print(f"Training random split: {epoch}")
+        try:
+            train_split(random.randint(0, N_SPLITS-1), split_size)
+        except:
+            ## TODO: clear GPU memory
+            print("GPU memory full")
 
 if __name__ == '__main__':
+    #TODO: move constants to CLI params
     main()
     
--- generator/train_generator.py
+++ generator/train_generator.py
@@ -1,168 +1,137 @@
-import csv
 import json
-import os
 import random
 
 import numpy as np
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import LSTM, Dense, Dropout, BatchNormalization, Input, Concatenate
 from tensorflow.keras.optimizers import Adam
 
-from utils import parse_args
 
 REUSES = 3
 BATCH_SIZE = 128
 EPOCHS = 50
-N_CATEGORIES = 13405
+N_CATEGORIES = 3205
 PRED_LENGTH = 15
-N_SPLITS = 5
+N_SPLITS = 100
+TRAIN_SPLITS = 50
 
 X_FILE = 'X_data.json'
-C_FOLDER = 'categories_large'
-MODEL_NAME = 'marko_gen_large.h5'
+C_FILE = '../categories_medium.txt'
+MODEL_NAME = 'marko_gene_medium.h5'
 
 EMPTY_CHAR = '#'
 CHARS = ' abcdefghijklmnopqrstuvwxyzåäö.' + EMPTY_CHAR
+N_CHARS = len(CHARS)
+CHAR_TO_INT = dict((w, i) for i, w in enumerate(CHARS))
 
-CATEGORY_BATCH_SIZE = 5000
 
-FILES = ['0.csv',
-         '1.csv',
-         '2.csv',
-         '3.csv',
-         '4.csv',
-         '5.csv',
-         '6.csv',
-         '7.csv',
-         '8.csv',
-         '9.csv',
-         '10.csv',
-         '11.csv',
-         '12.csv',
-         '13.csv',
-         '14.csv',
-         '15.csv',
-         '16.csv',
-         '17.csv',
-         '18.csv',
-         '19.csv',
-         '20.csv',
-         '21.csv',
-         '22.csv',
-         '23.csv',
-         '24.csv',
-         '25.csv',
-         '26.csv',
-         '27.csv',
-         '28.csv',
-         '29.csv',
-         '30.csv',
-         '31.csv',
-         '32.csv',
-         '33.csv',
-         '34.csv',
-         '35.csv',
-         '36.csv',
-         '37.csv',
-         '38.csv',
-         '39.csv'
-         ]
-
-def main():    
-    with open(X_FILE, 'r', encoding='utf-8') as json_file:
-        data = json.load(json_file)
-    messages = [x['formated'] for x in data]
-
-    n_chars = len(CHARS)
-
-    char_to_int = dict((w, i) for i, w in enumerate(CHARS))
-    int_to_char = dict((i, w) for i, w in enumerate(CHARS))
-
-### KAKSI INPUTTIA
-    # Input
+def create_model():
     inputA = Input(shape=(N_CATEGORIES,))
-    inputB = Input(shape=(PRED_LENGTH, n_chars))
+    inputB = Input(shape=(PRED_LENGTH, N_CHARS))
     # Categories
     x = Dense(100, activation='relu')(inputA)
     x = BatchNormalization()(x)
     x = Model(inputs=inputA, outputs=x)
     # Chars
-    y = LSTM(PRED_LENGTH * n_chars + 1, activation='relu', return_sequences=True)(inputB)
+    y = LSTM(PRED_LENGTH * N_CHARS + 1, activation='relu', return_sequences=True)(inputB)
     y = BatchNormalization()(y)
     y = Dropout(0.2)(y)
-    y = LSTM(PRED_LENGTH * n_chars + 1, activation='relu', return_sequences=True)(inputB)
+    y = LSTM(PRED_LENGTH * N_CHARS + 1, activation='relu', return_sequences=True)(inputB)
     y = BatchNormalization()(y)
     y = Dropout(0.2)(y) 
-    y = LSTM(PRED_LENGTH * n_chars + 1, activation='relu', return_sequences=False)(inputB)
+    y = LSTM(PRED_LENGTH * N_CHARS + 1, activation='relu', return_sequences=False)(inputB)
     y = BatchNormalization()(y)
     y = Dropout(0.2)(y)
     y = Model(inputB, outputs=y)
     # Combine
-    combined = Concatenate()([x.output, y.output])    
-    z = Dense(n_chars, activation='relu')(combined)
-    z = Dense(n_chars, activation='softmax')(z)
+    combined = Concatenate()([x.output, y.output])
+    z = Dense(N_CHARS, activation='relu')(combined)
+    z = Dense(N_CHARS, activation='softmax')(z)
     # Model
     model = Model(inputs=[x.input, y.input], outputs=z)
     opt = Adam(learning_rate=0.00005)
-    # model.compile(opt, 'mse', metrics=['accuracy'])
     model.compile(opt, 'categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def load_clf_model(model_name: str):
+    #TODO: use folder instead of file
+    #TODO: find newest model file from folder
+    try:
+        model = load_model(model_name)
+        print(f"Loaded newest model: {model_name}")
+    except:
+        model = create_model()
+        print("Training new model from scratch")
+
+    return model
+
+def train_split(split_idx: int, split_size: int):
+    print(f"Training random index: {split_idx}")
+    model = load_clf_model(MODEL_NAME)
     
-    file_idx = 0
-    for reuse_idx in range(REUSES):
-        files = os.listdir(C_FOLDER)
-        random.shuffle(files)
-        for filename in files:
-            with open(os.path.join(C_FOLDER, str(filename))) as f:
-                reader = csv.reader(f , delimiter=',')
-                categories = list(reader)
-            categories = np.array(categories).astype(float)
-            
-            XA = []
-            XB = []
-            y = []
-            for msg_idx, msg in enumerate(messages[file_idx*CATEGORY_BATCH_SIZE : (file_idx + 1)*CATEGORY_BATCH_SIZE]):
-                msg_str = EMPTY_CHAR*PRED_LENGTH + msg.strip()
-                for char_idx, char in enumerate(msg_str[:-1]):
-                    if char_idx < PRED_LENGTH - 1:
-                        continue
-
-                    xx = []
-                    for i in reversed(range(PRED_LENGTH)):
-                        xx.append(to_categorical(char_to_int[msg_str[char_idx-i]], n_chars))
-                    
-                    if len(categories[msg_idx]) != N_CATEGORIES:
-                        continue
-                    if max(categories[msg_idx]) == 0:
-                        continue
-                    XA.append(categories[msg_idx])
-                    XB.append(xx)
-                    y.append(to_categorical(char_to_int[msg_str[char_idx + 1]], n_chars))
-
-            XA = np.array(XA).astype(float)
-            XB = np.array(XB).astype(float)
-            y = np.array(y).astype(float)
-
-            split_size = len(XA) // N_SPLITS
-            for split_idx in range(N_SPLITS):
-                XA_split = XA[split_idx*split_size : ((split_idx+1)*split_size) - 1]
-                XB_split = XB[split_idx*split_size : ((split_idx+1)*split_size) - 1]
-                y_split = y[split_idx*split_size : ((split_idx+1)*split_size) - 1]
-                
-                model.fit(x=[XA_split, XB_split],
-                          y=y_split,
-                          epochs=EPOCHS,
-                          batch_size=BATCH_SIZE,
-                          shuffle=True)
-
-        if file_idx < len(FILES):
-            file_idx = file_idx + 1
-        else:
-            file_idx = 0
+    with open(X_FILE, 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+    messages = [x['formated'] for x in data]
+    
+    split_messages = messages[split_idx*split_size : ((split_idx+1)*split_size) - 1]
+    del messages
+    
+    with open(C_FILE, 'r') as f:
+        categories = f.readlines()
         
+    split_categories = categories[split_idx*split_size : ((split_idx+1)*split_size) - 1]
+    del categories
+    
+    XA = []
+    XB = []
+    y = []
+    for msg_idx, msg in enumerate(split_messages):
+        msg_str = EMPTY_CHAR*PRED_LENGTH + msg.strip()
+        for char_idx, char in enumerate(msg_str[:-1]):
+            if char_idx < PRED_LENGTH - 1:
+                continue
+
+            xx = []
+            for i in reversed(range(PRED_LENGTH)):
+                xx.append(to_categorical(CHAR_TO_INT[msg_str[char_idx-i]], N_CHARS))
+            
+            if len(split_categories[msg_idx]) != N_CATEGORIES:
+                continue
+            if max(split_categories[msg_idx]) == 0:
+                continue
+            XA.append(split_categories[msg_idx])
+            XB.append(xx)
+            y.append(to_categorical(CHAR_TO_INT[msg_str[char_idx + 1]], N_CHARS))
+
+    XA = np.array(XA).astype(float)
+    XB = np.array(XB).astype(float)
+    y = np.array(y).astype(float)
+        
+    model.fit(x=[XA, XB],
+              y=y,
+              epochs=EPOCHS,
+              batch_size=BATCH_SIZE,
+              shuffle=True)
+    
     model.save(MODEL_NAME)
 
+def main():    
+    with open(C_FILE, 'r') as f:
+        categories = f.readlines()
+
+    split_size = len(categories) // N_SPLITS
+    del categories
+
+    for epoch in range(TRAIN_SPLITS):
+        print(f"Training random split: {epoch}")
+        try:
+            train_split(random.randint(0, N_SPLITS-1), split_size)
+        except:
+            ## TODO: clear GPU memory
+            print("GPU memory full")
 
 if __name__ == '__main__':
-    args = parse_args()
+    #TODO: move constants to CLI params
     main()
